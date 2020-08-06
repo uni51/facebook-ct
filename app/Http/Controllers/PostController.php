64c10b78d18a8cc9eae6676b9d08f36fa03e43cc
uni51 <uni51@users.noreply.github.com>
2020-08-06 11:21:01 +0900

    8. Asserting on the Response

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
     public function store()
     {
-        return response([], 201);
+        $data = request()->validate([
+           'data.attributes.body' => '',
+        ]);
+
+        // dd($data);
+
+        $post = request()->user()->posts()->create($data['data']['attributes']);
+
+        return response([
+            'data' => [
+                'type' => 'posts',
+                'post_id' => $post->id,
+                'attributes' => [
+                    'body' => $post->body,
+                ]
+            ],
+            'links'=> [
+                'self' => url('/posts/'.$post->id),
+            ]
+        ], 201);
     }
 }
--- tests/Feature/PostToTimelineTest.php
+++ tests/Feature/PostToTimelineTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Post;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -26,8 +27,23 @@ class PostToTimelineTest extends TestCase
             ]
         ]);
 
-        $post = \App\Post::first();
+        $post = Post::first();
 
-        $response->assertStatus(201);
+        $this->assertCount(1, Post::all());
+        $this->assertEquals($user->id, $post->user_id);
+        $this->assertEquals('Testing Body', $post->body);
+        $response->assertStatus(201)
+            ->assertJson([
+               'data' => [
+                   'type' => 'posts',
+                   'post_id' => $post->id,
+                   'attributes' => [
+                       'body' => 'Testing Body',
+                   ]
+               ],
+                'links'=> [
+                    'self' => url('/posts/'.$post->id),
+                ]
+            ]);
     }
 }
