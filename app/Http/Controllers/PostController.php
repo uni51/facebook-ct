3fd18e1676854670c5a0e5adc9e233b24bb5c0ff
uni51 <uni51@users.noreply.github.com>
2020-08-07 09:57:30 +0900

    Refactor to Post & User Resources

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Http\Resources\Post as PostResource;
 
 class PostController extends Controller
 {
@@ -16,17 +17,6 @@ class PostController extends Controller
 
         $post = request()->user()->posts()->create($data['data']['attributes']);
 
-        return response([
-            'data' => [
-                'type' => 'posts',
-                'post_id' => $post->id,
-                'attributes' => [
-                    'body' => $post->body,
-                ]
-            ],
-            'links'=> [
-                'self' => url('/posts/'.$post->id),
-            ]
-        ], 201);
+        return new PostResource($post);
     }
 }
